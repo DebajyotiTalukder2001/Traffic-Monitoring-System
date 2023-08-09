1872d7ee5c5f861753af2d067d52126e5a3e8b31
DebajyotiTalukder2001 <136104351+DebajyotiTalukder2001@users.noreply.github.com>
2023-08-09 19:58:41 +0530

    My TMS Project

--- Traffic-Monitoring-System/test.py
+++ Traffic-Monitoring-System/test.py
@@ -29,12 +29,11 @@ class_list = data.split("\n")
 #print(class_list)
 
 count=0
-
+speed={}
 area=[(225,335),(803,335),(962,408),(57,408)]
 area_c=set()
 tracker=Tracker()
-
-
+speed_limit=57
 
 while True:    
     ret,frame = cap.read()
@@ -70,11 +69,37 @@ while True:
         cx=int(x3+x4)//2
         cy=int(y3+y4)//2
         results=cv2.pointPolygonTest(np.array(area,np.int32),((cx,cy)),False)
+        
         if results>=0:
            cv2.circle(frame,(cx,cy),4,(0,0,255),-1)
-           cv2.putText(frame,str(id),(cx,cy),cv2.FONT_HERSHEY_COMPLEX,0.6,(0,255,255),1,cv2.LINE_AA)
+           cv2.putText(frame,str(id),(x3,y3),cv2.FONT_HERSHEY_COMPLEX,0.6,(0,255,255),1,cv2.LINE_AA)
            cv2.rectangle(frame,(x3,y3),(x4,y4),(0,0,255),2)
            area_c.add(id)
+           now = time.time()
+           if id not in speed:
+               speed[id]=now
+           else:
+               prev_time = speed[id]
+               speed[id]=now
+               dist = 5
+               a_speed_ms = dist / (now - prev_time)
+               a_speed_kh = a_speed_ms * 3.6
+               cv2.putText(frame,str(int(a_speed_kh))+'Km/h',(x4,y4),cv2.FONT_HERSHEY_COMPLEX,0.8,(0, 255, 0),2,cv2.LINE_AA)
+               speed[id]=now
+               # Check if the speed exceeds the speed limit
+               if a_speed_kh >= speed_limit:
+            # Display a warning message
+                  cv2.putText(frame,"Speed limit violated!",(440,115),cv2.FONT_HERSHEY_TRIPLEX,0.8,(255,0,255),2,cv2.LINE_AA)
+                   # Display the message for 3 seconds
+                  start_time = time.time()
+                  while time.time() - start_time < 3:
+                     cv2.imshow("RGB", frame)
+                     if cv2.waitKey(1) & 0xFF == 27:
+                         break
+           
+           
+           
+           
         
         
     cv2.polylines(frame,[np.array(area,np.int32)],True,(0, 255, 0),2)
